6c3590f7e801a9b5de69828b37b773953ae44e5b
LinkMeir <meirhadad90@gmail.com>
2026-02-10 21:14:20 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,36 @@
-const CACHE_NAME = 'link-meir-v1';
-const ASSETS = ['./', './index.html', './styles.css', './app.js', './firebase-config.js', './manifest.json', './icon.png'];
+const CACHE_NAME = 'link-manager-single-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
 
+// התקנה: שמירת הקבצים בזיכרון המטמון
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
+// שליפה: קודם כל נסה רשת, אם אין אינטרנט - תביא מהמטמון
 self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
+
+// ניקוי מטמון ישן כשמעדכנים גרסה
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
 });
